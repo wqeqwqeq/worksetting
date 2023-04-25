8eeabc44a69af232722c7c80afd5589e16ce7e58
wqeqwqeq <49264762+wqeqwqeq@users.noreply.github.com>
2023-04-24 23:00:04 -0500

    Create package_control_setting.js

---
+++ package_control_setting.js
@@ -0,0 +1,31 @@
+{
+	"bootstrapped": true,
+	"in_process_packages":
+	[
+	],
+	"installed_packages":
+	[
+		"AutoDocstring",
+		"BracketHighlighter",
+		"Compare Side-By-Side",
+		"DictionaryAutoComplete",
+		"Expand Selection to Quotes",
+		"HTMLBeautify",
+		"Indent XML",
+		// "SublimeREPL",
+    "Terminus",
+		"JsFormat",
+		"Package Control",
+		"PackageDev",
+		"python-black",
+		"SAS Syntax and Theme",
+		"Terminus",
+	],
+	"ignore_vcs_packages":true,
+	"channels":
+	[
+		"https://packagecontrol.io/channel_v3.json",
+		// "https://github.carmax.com/CarMax/sublime_channel",
+	],
+
+}
